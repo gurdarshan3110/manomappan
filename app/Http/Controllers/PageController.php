1621d23d478cf42c31fb849e14effa384d841f52
Daman Mokha <damanmokha24@gmail.com>
2025-05-22 23:06:37 +0530

    Add meta tags configuration and update PageController methods for SEO

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -8,6 +8,31 @@ class PageController extends Controller
 {
     public function home()
     {
-        return view('pages.home');
+        $meta = config('metatags.home');
+        return view('pages.home', compact('meta'));
+    }
+
+    public function login()
+    {
+        $meta = config('metatags.login');
+        return view('pages.login', compact('meta'));
+    }
+
+    public function register()
+    {
+        $meta = config('metatags.register');
+        return view('pages.register', compact('meta'));
+    }
+
+    public function about()
+    {
+        $meta = config('metatags.about');
+        return view('pages.about', compact('meta'));
+    }
+
+    public function contact()
+    {
+        $meta = config('metatags.contact');
+        return view('pages.contact', compact('meta'));
     }
 }
---
+++ config/metatags.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+    'home' => [
+        'title' => 'Manomaapan - Career Counselling & Guidance Platform',
+        'description' => 'Find clarity in your career through self-insight and expert guidance. Take career counselling assessment to uncover your strengths.',
+        'keywords' => 'career counselling, career guidance, career assessment, student counselling, career path, career development',
+        'author' => 'Manomaapan',
+    ],
+    'login' => [
+        'title' => 'Login - Manomaapan',
+        'description' => 'Access your Manomaapan account to explore career counselling and guidance resources.',
+        'keywords' => 'login, manomaapan, career counselling, career guidance',
+        'author' => 'Manomaapan',
+    ],
+    'register' => [
+        'title' => 'Register - Manomaapan',
+        'description' => 'Join Manomaapan to start your journey in career counselling and guidance.',
+        'keywords' => 'register, manomaapan, career counselling, career guidance',
+        'author' => 'Manomaapan',
+    ],
+    'about' => [
+        'title' => 'About Us - Manomaapan',
+        'description' => 'Learn more about Manomaapan and our mission to provide career counselling and guidance.',
+        'keywords' => 'about, manomaapan, career counselling, career guidance',
+        'author' => 'Manomaapan',
+    ],
+    'contact' => [
+        'title' => 'Contact Us - Manomaapan',
+        'description' => 'Get in touch with Manomaapan for career counselling and guidance inquiries.',
+        'keywords' => 'contact, manomaapan, career counselling, career guidance',
+        'author' => 'Manomaapan',
+    ],
+];
